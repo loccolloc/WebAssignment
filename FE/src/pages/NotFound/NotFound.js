8d8ce44b9ca46cc7aeb27179aa7a53670fb1b2a5
LuVe132003 <vu.le132003@hcmut.edu.vn>
2023-12-14 20:22:31 +0700

    footer and notfound css

--- FE/src/pages/NotFound/NotFound.js
+++ FE/src/pages/NotFound/NotFound.js
@@ -7,7 +7,7 @@ export default function NotFound() {
         <div><section className={styled.page_404}>
             <div className="container">
                 <div className="row">
-                    <div className="col-sm-12 ">
+                    <div className="col-sm-12 flex justify-center">
                         <div className="col-sm-10 col-sm-offset-1  text-center">
                             <div className={styled.four_zero_four_bg}>
                                 <h1 className="text-center ">404</h1>
